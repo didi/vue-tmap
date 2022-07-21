df4ef243effcbb9b1de9141a044899e67e4087f8
baoluoqiang <baoluoqiang@didiglobal.com>
2022-07-21 17:57:33 +0800

    feat: 支持在乾坤子应用中使用

--- src/utils/loadSDK.ts
+++ src/utils/loadSDK.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable no-underscore-dangle */
 export default function loadSDK(
   version: string,
   key: string,
@@ -12,6 +14,14 @@ export default function loadSDK(
     window.tmapCallback = function tmapCallback() {
       resolve(window.TMap);
     };
+
+    // 在乾坤子应用中使用
+    if ((window as any).__POWERED_BY_QIANKUN__ && top) {
+      top.tmapCallback = function tmapCallback() {
+        resolve(TMap);
+      };
+    }
+
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = `https://map.qq.com/api/gljs?v=${version}&key=${key}&libraries=${libs.join(
