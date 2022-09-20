a5997e8685586813e0b0a15415456e59b3b27fd6
baoluoqiang <baoluoqiang@didiglobal.com>
2022-09-20 15:31:09 +0800

    feat: 修改构建工具为 vite

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+// 文档: https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      packages: resolve(__dirname, './packages'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'vue-tmap',
+      fileName: (format) => `vue-tmap.${format}.js`,
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ['vue'],
+    },
+  },
+  plugins: [dts()],
+});
